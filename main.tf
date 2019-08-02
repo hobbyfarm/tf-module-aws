a67c0764f967656dcda9e3ded2cf01475437d3f2
Chris Kim <oats87g@gmail.com>
2019-08-02 11:01:58 -0700

    t2 large

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_key_pair" "instance_keypair" {
 
 resource "aws_instance" "instance" {
   ami           = "${var.image}"
-  instance_type = "t2.medium"
+  instance_type = "t2.large"
   key_name = "${aws_key_pair.instance_keypair.key_name}"
   subnet_id = "${var.subnet}"
   vpc_security_group_ids = ["${var.vpc_security_group_id}"]
